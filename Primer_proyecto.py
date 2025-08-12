ec4360590f7595b144a833d896964be7596f0442
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-08-11 22:01:48 -0400

    commit Nro 6 "dia 1"

--- Primer_proyecto.py
+++ Primer_proyecto.py
@@ -17,3 +17,15 @@ print("Esta es una linea \ny esta es otra linea ")
 print("\testa es la tercera linea")
 print("This isn'nt a number ")
 print("esta signo \\ es una barra ")
+
+#Ejercicio 4
+print("Línea 1\nLínea 2\nLínea 3")
+
+#Ejercicio 5
+print("A\tB\tC")
+print("D\tE\tF")
+print("G\tH\tI")
+
+#Ejercicio 6
+print("Barra Normal: /")
+print("Barra Invertida: \\")
