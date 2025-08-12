a5a3a3112c5a43c041978bd845eb49ef24a5fce7
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-08-11 21:47:57 -0400

    commit Nro 4 "dia 1"

--- Primer_proyecto.py
+++ Primer_proyecto.py
@@ -10,3 +10,9 @@ print('Estudiar con "Python Total" es super divertido')
 print(100 + 455)
 
 # Strings
+print("hola" + " Seba")
+print("Me llamo \"Seba\"")
+print("Esta es una linea \ny esta es otra linea ")
+print("\testa es la tercera linea")
+print("This isn'nt a number ")
+print("esta signo \\ es una barra ")
