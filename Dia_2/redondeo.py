f0a5a005059032f37ee4c19cf165d9ed5a19d573
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-08-12 22:01:53 -0400

    commit Nro 7 "Dia 2"

---
+++ Dia_2/redondeo.py
@@ -0,0 +1,19 @@
+print(90/7)
+print(round(90/7))
+
+resultado = round(90/7)
+print(resultado)
+
+
+valor = round(95.66666666666, 2)
+print(valor)
+
+#Ejercicio 1
+print(round((10/3), 2))
+
+#Ejercicio 2
+valor = 10.676767
+print(round(valor))
+
+#Ejercicio 3
+print(round((5**0.5), 4))
