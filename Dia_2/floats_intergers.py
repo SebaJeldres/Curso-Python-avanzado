e9bb0b604f15512db5eb7dff076c186e7e3f2dee
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-08-12 21:08:06 -0400

    commit Nro 2 "Dia 2", Restructuracion de la carpeta

---
+++ Dia_2/floats_intergers.py
@@ -0,0 +1,2 @@
+#Intergers y floats
+
---
+++ Dia_2/variables.py
@@ -0,0 +1,45 @@
+#Tipos de datos
+#Strings 'str' = "Hola", "#$%&", " ", "123"
+#Interger 'int' = 12, -12, 0
+#Floating 'float' = 12.98, 23.0, -87.65
+
+#Variables
+nombre = "Seba"
+print(nombre)
+
+nombre = "Juan"
+print(nombre)
+
+edad = 21
+print(edad)
+
+edad2 = 23
+
+edad3 = edad + edad2
+print(edad3)
+
+nombre = input("Dime tu nombre\n")
+print("Tu nombre es", nombre)
+
+#Ejercicio 1
+nombre = "Tony Soprano"
+edad = 51
+
+#Ejercicio 2
+nombre = "Julia"
+apellido = "Roberts"
+
+nombrecompleto = nombre + " " + apellido
+
+#Ejercicio 3
+curso = "Python"
+print("Estás tomando un curso de", curso)
+
+
+#Nombre de las variables
+#Tiene que ser legible
+#nombre de unidad "Edad_Completa"
+#Cuidado con la ñ y las tilder
+#No se puede iniciar con numeros
+#sin signos
+#no palabras claves
