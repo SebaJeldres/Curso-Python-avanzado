0e34db3d552c5e2f4c352f9fc77ffeac82618ef4
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-08-13 21:56:02 -0400

    commit Nro 3 "Dia 3", Strings

---
+++ Dia_3/Strings.py
@@ -0,0 +1,43 @@
+#inmutables
+#concatebables
+#multiplicables
+#Multilineales
+#verificar si contiene
+#Contar caracteres
+
+
+#Multiplicador de cadenas de texto
+n1 = "Carina"
+n2 = "na"
+print(n1 * 10)
+
+#Salto de linea sin \n
+poema = """mil pequeños peces blancos
+como si hirviera el
+color del agua"""
+
+print(poema)
+
+#devuelve false o true
+print("agua" in poema)
+
+#muestra la cantidad de caracteres
+print(len(poema))
+
+#Ejercicio 1
+palabra = "Repetición"
+
+print(palabra * 15)
+
+#Ejercicio 2
+frase = """ Tierra mojada
+mis recuerdos de viaje,
+entre las lluvias
+"""
+
+print("agua" not in  frase)
+
+#Ejercicio 3
+palabra = "electroencefalografista"
+
+print(len(palabra))
