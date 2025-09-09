650676c9d85863453ab18a0afca33b3477a70ac7
SebaJeldres <Sjeldresosoriodev@gmail.com>
2025-09-08 21:27:44 -0300

    commit 4 dia 3 "Boolean"

---
+++ Dia_3/Boolean.py
@@ -0,0 +1,36 @@
+# solo verdadero o falso
+# si se usa < > = >= <= != se retornara un booleano
+
+var1 = True
+var2 = False
+print(var1, var2)
+
+
+numero = 5 > 3
+print(numero)
+
+numero2 = 5 < 3
+print(numero2)
+
+numero3 = 5 == 3
+print(numero3)
+
+lista = [1, 2, 3]
+control = 1 in lista
+print(control)
+
+
+#Ejercicios
+
+#Ejercicio 1
+comparacion = 5 > 3
+prueba = comparacion
+
+#Ejercicio 2
+a = 17834/34 > 87*56
+
+print(a)
+
+#Ejercicio 3
+a = (25**0.5) == 5
+print(a)
